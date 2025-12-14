a8731033562556db5a8465dd9fe493a79b7a5d57
Övünç Aygün <ovunc.aygun@gmail.com>
2025-12-14 13:28:48 +0300

    Module 6 initialization

---
+++ include/MSH_M6.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "ISecurityManager.h"
+
+class MSH_M6 {
+public:
+    MSH_M6();
+    ~MSH_M6();
+
+    ISecurityManager* getISecurityManager();
+};
---
+++ src/MSH_M6.cpp
@@ -0,0 +1,8 @@
+#include "MSH_M6.h"
+
+MSH_M6::MSH_M6() = default;
+MSH_M6::~MSH_M6() = default;
+
+ISecurityManager* MSH_M6::getISecurityManager() {
+    return NULL;
+}
