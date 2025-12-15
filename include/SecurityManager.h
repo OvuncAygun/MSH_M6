604b99b7ae04b7e09cc757b2b9a411da794598c4
roa-aw <rouahawata@gmail.com>
2025-12-15 19:55:28 +0300

    Implement SecurityManager interface and state variables

---
+++ include/SecurityManager.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "ISecurityManager.h"
+
+class IEmergencyHandler;
+
+class SecurityManager : public ISecurityManager {
+private:
+    bool systemArmed;
+    bool alarmActive;
+    bool stopFlag;
+
+    IEmergencyHandler* motionChain;
+    IEmergencyHandler* smokeChain;
+
+public:
+    SecurityManager();
+    virtual ~SecurityManager();
+
+    bool getIsSystemArmed();
+    void setIsSystemArmed(bool isSystemArmed);
+
+    bool getIsAlarmActive();
+    void setIsAlarmActive(bool isAlarmActive);
+
+    void onMotionDetected();
+    void onSmokeDetected();
+    void stopSequence();
+
+    bool isSequenceStopped() const;
+
+    void setMotionChain(IEmergencyHandler* handler);
+    void setSmokeChain(IEmergencyHandler* handler);
+};
