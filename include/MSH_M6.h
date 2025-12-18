828197fdaf7001a24a0de6e9a6ecfb068eb2e357
Övünç Aygün <ovunc.aygun@gmail.com>
2025-12-18 14:57:36 +0300

    Added integration methods

--- include/MSH_M6.h
+++ include/MSH_M6.h
@@ -1,11 +1,19 @@
 #pragma once
 
 #include "ISecurityManager.h"
+#include "ILogger.h"
 
 class MSH_M6 {
 public:
     MSH_M6();
     ~MSH_M6();
 
-    ISecurityManager* getISecurityManager();
+    void setLogger(ILogger* logger);
+    void initializeSecurityManager();
+
+    ISecurityManager* getSecurityManager();
+
+private:
+    ISecurityManager* securityManager;
+    ILogger* logger;
 };
--- src/MSH_M6.cpp
+++ src/MSH_M6.cpp
@@ -1,8 +1,20 @@
 #include "MSH_M6.h"
+#include "SecurityChainBuilder.h"
+#include "SecurityManager.h"
 
 MSH_M6::MSH_M6() = default;
 MSH_M6::~MSH_M6() = default;
 
-ISecurityManager* MSH_M6::getISecurityManager() {
+void MSH_M6::setLogger(ILogger* logger) {
+    this->logger = logger;
+}
+
+void MSH_M6::initializeSecurityManager() {
+    securityManager = new SecurityManager();
+    SecurityChainBuilder builder;
+    builder.build(*static_cast<SecurityManager*>(securityManager));
+}
+
+ISecurityManager* MSH_M6::getSecurityManager() {
     return 0;
 }
